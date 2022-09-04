9cbb33f9ebd44cefe726dd248fed1c01611e4baf
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-04 15:18:57 +0200

    Add project files.

---
+++ Company.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    internal abstract class Company
+    {
+        List<IEmployee> employees;
+
+        public abstract void     CreateEmployee();
+
+        public void CreateSoftware()
+        {
+            foreach (var employee in employees)
+            {
+                employee.DoWork();
+            }
+        }
+    }
+}
---
+++ Developer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern
+{
+    internal class Developer : IEmployee
+    {
+        public void DoWork()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
